308858a8a57f43d06a9e9f480b84f11c979854b5
sai3010 <saiprasanth2007@gmail.com>
2018-05-01 14:01:56 +0530

    done

--- js/index.js
+++ js/index.js
@@ -5,6 +5,16 @@ alert(ans);
 if(ans.match("Brooklyn 99"))
 {
     alert("Correct!! You will be redirected to the next question");
-    window.location="questions2.html";
+    window.location="question2.html";
 }
+}
+function check4()
+{
+    var ans=document.getElementById("qans4").value.toUpperCase();
+    if(ans.match("THANOS"))
+    {
+        alert("Correct!! You will be redirected to the next question");
+        window.location="question4.html";
+    }
+
 }
